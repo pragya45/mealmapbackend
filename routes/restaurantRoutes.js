07785e78677f49518fba6a5897c83e7808f77ccc
Pragya45 <pphuyal170@gmail.com>
2024-06-22 12:33:39 +0545

    search done

--- routes/restaurantRoutes.js
+++ routes/restaurantRoutes.js
@@ -2,10 +2,9 @@ const express = require('express');
 const { getRestaurants, getRestaurantById, addRestaurant, searchRestaurants } = require('../controllers/restaurantController');
 const router = express.Router();
 
+router.get('/search', searchRestaurants); // Define the search route first
 router.get('/', getRestaurants);
 router.get('/:id', getRestaurantById);
 router.post('/', addRestaurant);
-router.get('/search', searchRestaurants);
-
 
 module.exports = router;
